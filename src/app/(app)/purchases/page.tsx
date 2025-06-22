200b3957a07e91d29d221493e2fcff146f83c612
Trần Văn Thuận Nguyễn <thuanntv013@gmail.com>
2025-06-22 20:03:01 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(app)/purchases/page.tsx
+++ src/app/(app)/purchases/page.tsx
@@ -58,6 +58,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import {
     Select,
